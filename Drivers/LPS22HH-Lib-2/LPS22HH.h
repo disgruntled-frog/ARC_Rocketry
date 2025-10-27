e7f58746541c9964f290849933214459c829daf1
disgruntled-frog <manleysam64@gmail.com>
2025-10-26 21:45:37 -0500

    Commiting the latest updates to header file. Finishing tmr.

--- Drivers/LPS22HH-Lib-2/LPS22HH.h
+++ Drivers/LPS22HH-Lib-2/LPS22HH.h
@@ -5,9 +5,32 @@
 #ifndef LPS22HH_H
 #define LPS22HH_H
 
-
+#include "Arduino.h"
 
 class LPS22HH {
+    public:
+        LPS22HH();
+
+        void config_baro();
+        void read_baro(double& baro);
+        double get_alt();
+    private:
+        double alt;
+
+        // Internal Functions
+        byte readRegisters(byte _addr, byte _reg, byte* _value,byte _size = 1);
+        byte writeRegister(byte _addr, byte _reg, byte _value);
+
+        // Registers
+        int whoami_reg = 0x0F;
+        int ctrl1_reg  = 0x10;
+        int out_xl_reg = 0x28;
+        int out_l_reg  = 0x29;
+        int out_h_reg  = 0x2A;
+
+        // Values
+        int whoami_val = 0xB3;
+
 
 };
 

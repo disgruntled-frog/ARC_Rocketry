2f976faba8527d69946a52953d466115732d5de1
disgruntled-frog <manleysam64@gmail.com>
2025-10-30 20:35:24 -0500

    Should be good. hard coded config.

--- Drivers/LPS22HH-Lib-2/LPS22HH.cpp
+++ Drivers/LPS22HH-Lib-2/LPS22HH.cpp
@@ -3,3 +3,50 @@
 //
 
 #include "LPS22HH.h"
+
+
+void LPS22HH::config_baro(){
+    // This will be default configuration for everyone, future plans to add customization
+    byte results = writeRegister(baro_addr, ctrl1_reg, 0b01110000); // ODR 200 Hz, No Low pass, Coninuous Mode, 4-wire SPI
+}
+
+void LPS22HH::read_baro(byte* _value, int32_t& raw_baro, double& baro){
+    readRegisters(baro_addr,out_xl_reg,baro_data,baro_size);
+
+    // Byte Concatenation
+    raw_baro = (baro_data[2]<<16)|(baro_data[1]<<8)|(baro_data[0]);  // baro_data == [LSB,midSB,MSB]
+    // Convert to hPa
+    baro = raw_baro / 4096.0;
+
+}
+
+byte LPS22HH::readRegisters(byte _addr, byte _reg, byte* _value,byte _size){
+    // byte _addr: address of the I2C sensor you wish to communicate with
+    // byte _reg: address of the register within said I2C sensor you wish to read from
+    // byte* _value: pointer to the array you will store the value(s) read
+    // byte _size: size of the value array
+    // byte returnval: error code, 0 is normal operation
+
+    Wire.beginTransmission(_addr);
+    Wire.write(_reg);
+    Wire.endTransmission(false);
+    Wire.requestFrom(_addr, _size);
+
+    while(Wire.available()){
+        *_value = Wire.read();
+        _value++;
+    }
+
+    return 0;
+}
+
+byte LPS22HH::writeRegister(byte _addr, byte _reg, byte _value){
+    // byte _addr: address of the I2C sensor you wish to communicate with
+    // byte _reg: address of the register within said I2C sensor you wish to write to
+    // byte _value: byte you wish to write to said register
+
+    Wire.beginTransmission(_addr);
+    Wire.write(_reg);
+    Wire.write(_value);
+    return Wire.endTransmission();
+}
--- Drivers/LPS22HH-Lib-2/LPS22HH.h
+++ Drivers/LPS22HH-Lib-2/LPS22HH.h
@@ -6,14 +6,16 @@
 #define LPS22HH_H
 
 #include "Arduino.h"
+#include <Wire.h>
 
 class LPS22HH {
     public:
         LPS22HH();
 
         void config_baro();
-        void read_baro(double& baro);
+        void read_baro(byte* _value, int32_t& raw_baro, double& baro);
         double get_alt();
+
     private:
         double alt;
 
@@ -21,16 +23,22 @@ class LPS22HH {
         byte readRegisters(byte _addr, byte _reg, byte* _value,byte _size = 1);
         byte writeRegister(byte _addr, byte _reg, byte _value);
 
+        // Device Address
+        const int baro_addr  = 0x5C;
+
         // Registers
-        int whoami_reg = 0x0F;
-        int ctrl1_reg  = 0x10;
-        int out_xl_reg = 0x28;
-        int out_l_reg  = 0x29;
-        int out_h_reg  = 0x2A;
+        const int whoami_reg = 0x0F;
+        const int ctrl1_reg  = 0x10;
+        const int out_xl_reg = 0x28;
+        const int out_l_reg  = 0x29;
+        const int out_h_reg  = 0x2A;
 
         // Values
-        int whoami_val = 0xB3;
+        const int whoami_val = 0xB3;
 
+        // Data
+        static constexpr byte baro_size = 3;
+        byte baro_data[baro_size] = {};
 
 };
 
